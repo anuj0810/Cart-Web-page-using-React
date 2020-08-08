85d524b395fdc506ba78937f725854bd1024838f
anuj0810 <anujagrawal@outlook.in>
2020-08-09 04:57:49 +0530

    add increase button functionality

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,62 @@
 import React from 'react';
 class CartItem extends React.Component{   //  its a class base component
-render(){
+    constructor(){
+        super()
+        this.state={
+            price: 1100,
+            title : 'Smart Phone',
+            Qty : 0
+
+        }
+        // this.increaseQuantity=this.increaseQuantity.bind(this); 
+    }
+  increaseQuantity=()=>{
+        console.log('this.state',this.state);
+       //setState() funtion inherit form React.component and help to update the value in state object
+       //setState form 1
+    //    this.setState({
+    //        Qty:this.state.Qty+1 // this process is called shallow merging
+    //    })
+
+       //setState form 2
+
+       this.setState((prevState)=>{
+           return {
+               Qty:prevState.Qty+1
+           }
+        })
+      
+    }
+    render(){
+        const{price,title,Qty}=this.state;
     return(
-        <div clasName="cart-item">
+    
+        <div className="cart-item">
         <div className="left-block">
         <img style={styles.image}/>
         </div>
         <div className="right-block">
-            <div style={{color:'blue',fontSize:25}}>Phone</div>
-            <div  style={{color:'#777'}}>Rs: 999</div>
-            <div style={{color:'#777'}}>Qty: 1</div>
+            <div style={{color:'blue',fontSize:35}}>{title}</div>
+            <div  style={{color:'#777'}}>Rs: {price}</div>
+            <div style={{color:'#777'}}>Qty: {Qty}</div>
             <div className="cart-item-action">
                 {/* Button */}
-
+            <img  
+              alt="increase" 
+              className="action-icon" 
+              src="https://image.flaticon.com/icons/svg/992/992651.svg"
+              onClick = {this.increaseQuantity}
+            />
+            <img 
+              alt="decrease" 
+              className="action-icon" 
+              src="https://image.flaticon.com/icons/svg/1828/1828906.svg" 
+            />
+            <img 
+              alt="delete" 
+              className="action-icon" 
+              src="https://image.flaticon.com/icons/svg/1214/1214428.svg" 
+            />
             </div>
         </div>
 
@@ -23,8 +67,8 @@ render(){
 }
  const styles = {
      image:{
-         height:110,
-         width:110,
+         height:150,
+         width:150,
          borderRadius:4,
          background:'#ccc'
      }
